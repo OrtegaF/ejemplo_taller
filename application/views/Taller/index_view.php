2f6cb24d74e2f8f23ebb57492e04871833f64060
OrtegaF <ortegavaldesf@gmail.com>
2023-04-24 11:21:35 -0500

    Creacion de controlador Taller

---
+++ application/views/Taller/index_view.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Vista por defecto</h1>
+    <div>
+        <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Asperiores delectus suscipit tempora sunt molestiae quibusdam 
+            veritatis, libero enim nobis quaerat nisi pariatur consequatur 
+            cumque nemo voluptate ducimus tenetur. Vel, ipsam.
+        </p>
+    </div>
+</body>
+</html>
